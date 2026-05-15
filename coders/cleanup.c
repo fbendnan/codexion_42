12fba03c2e20d7db5441043cf3aa26304b058691
fbendnan <fatimazahrabendnane@gmail.com>
2026-05-15 08:27:22 +0100

    add

--- coders/cleanup.c
+++ coders/cleanup.c
@@ -6,14 +6,13 @@
 /*   By: fbendnane <fbendnane@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 13:30:42 by fbendnan          #+#    #+#             */
-/*   Updated: 2026/05/13 09:59:55 by fbendnane        ###   ########.fr       */
+/*   Updated: 2026/05/15 08:14:39 by fbendnane        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // gcc -Wall -Wextra -Werror -pthread *.c -o codexion
 // ./codexion 2 2000 100 100 100 2 50 fifo
 
-//cleanup.c
 #include "codexion.h"
 
 void cleanup(t_dongle *dongles, t_coder *coders, t_shared_info *info, t_simulation *sim)
@@ -25,12 +24,8 @@ void cleanup(t_dongle *dongles, t_coder *coders, t_shared_info *info, t_simulati
     i = 0;
     while (i < info->number_of_coders)
     {
-        // Destroy dongle mutex
         pthread_mutex_destroy(&dongles[i].mutex);
         pthread_cond_destroy(&dongles[i].cond);
-
-
-        // Destroy per-coder personal condition variable and mutex
         pthread_cond_destroy(&coders[i].personal_cond);
         pthread_mutex_destroy(&coders[i].personal_mutex);
 
--- coders/utils.c
+++ coders/utils.c
@@ -6,7 +6,7 @@
 /*   By: fbendnane <fbendnane@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 19:25:58 by fbendnan          #+#    #+#             */
-/*   Updated: 2026/05/13 11:18:21 by fbendnane        ###   ########.fr       */
+/*   Updated: 2026/05/15 07:25:42 by fbendnane        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,12 +64,12 @@ void precise_usleep(long time_in_ms, t_simulation *sim)
     while (get_time_in_ms() - start < time_in_ms)
     {
         pthread_mutex_lock(&sim->mutex);
-        if (!sim->running) // Check if we should stop early
+        if (!sim->running)
         {
             pthread_mutex_unlock(&sim->mutex);
             return;
         }
         pthread_mutex_unlock(&sim->mutex);
-        usleep(100); // Small chunks
+        usleep(100);
     }
 }
