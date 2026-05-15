d8616b92e54665abd2c8db22804203ee03803095
fbendnan <fatimazahrabendnane@gmail.com>
2026-05-15 08:30:39 +0100

    add

--- coders/cleanup.c
+++ coders/cleanup.c
@@ -6,7 +6,7 @@
 /*   By: fbendnane <fbendnane@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 13:30:42 by fbendnan          #+#    #+#             */
-/*   Updated: 2026/05/15 08:14:39 by fbendnane        ###   ########.fr       */
+/*   Updated: 2026/05/15 08:29:55 by fbendnane        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,6 @@ void cleanup(t_dongle *dongles, t_coder *coders, t_shared_info *info, t_simulati
         pthread_cond_destroy(&dongles[i].cond);
         pthread_cond_destroy(&coders[i].personal_cond);
         pthread_mutex_destroy(&coders[i].personal_mutex);
-
-        // Free any remaining request nodes in the dongle's wait queue
         tmp = dongles[i].wait_queue;
         while (tmp)
         {
@@ -39,17 +37,9 @@ void cleanup(t_dongle *dongles, t_coder *coders, t_shared_info *info, t_simulati
         }
         i++;
     }
-
-    // Destroy print mutex
     pthread_mutex_destroy(&info->print_mutex);
-
-    // Destroy simulation mutex
     pthread_mutex_destroy(&sim->mutex);
-
-    // Free the scheduler string
     free(info->scheduler);
-
-    // Free the arrays
     free(dongles);
     free(coders);
 }
